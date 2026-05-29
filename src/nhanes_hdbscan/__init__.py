2821113644d9e8e1e3480bcb0e74438133a3c0d3
KrishnaSaiPokala <KrishnaSaiPokala@users.noreply.github.com>
2026-05-29 13:50:35 -0400

    Add modular research package scaffold

---
+++ src/nhanes_hdbscan/__init__.py
@@ -0,0 +1,8 @@
+"""
+NHANES-HDBSCAN cardiometabolic phenotyping.
+
+A modular research software package for reproducible unsupervised
+cardiometabolic biomarker phenotyping using public NHANES data.
+"""
+
+__version__ = "0.1.0"
---
+++ tests/test_imports.py
@@ -0,0 +1,3 @@
+def test_package_imports():
+    import nhanes_hdbscan
+    assert nhanes_hdbscan is not None
